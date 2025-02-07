c07e26de12f7c6818024e90ed771bcc55f78a43f
Krishna <anonymous@google.com>
2025-02-07 19:50:59 +0530

    Responsiveness added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,50 +3,40 @@ import Dark from '../assets/dark.svg'
 import Light from '../assets/light.svg'
 import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
-// const lightMode = {
-//     color:"black",
-//     backgroundColor:"rgba(128, 128, 128, 0.3)"
-// }
-// const darkMode = {
-//     color:"white",
-//     backgroundColor:"black"
-// }
+
 export default function Navbar(props){
     const[svg,setSvg]=useState(Dark)
-    // const[mode,cngMode]=useState(lightMode)
     
     const handleSvg=()=>{
         if(svg===Dark){
             setSvg(Light);
-            // cngMode(darkMode)
         }
         else{
-            // cngMode(lightMode)
             setSvg(Dark);
         }
     }
     return(
         <>
-        <nav style={props.mode} className='w-[100vw] fixed top-0 h-[3.5em] bg-gray-500/30 backdrop-blur-lg flex justify-between items-center'>
-            <div className='text-2xl font-bold p-2.5'onClick={()=>window.location.reload()} >{props.title}</div>
-            <div className='flex font-bold pr-10'>
-                <NavLink to='/' className={({ isActive }) =>
-                props.mode.color=='black'?(isActive ? "text-black" : "text-gray-500"):(isActive ? "text-white" : "text-gray-500")                } end>
-                <div>Home</div>
-                </NavLink>
-                <NavLink to='/about'className={({ isActive }) =>
-                 props.mode.color=='black'?(isActive ? "text-black" : "text-gray-500"):(isActive ? "text-white" : "text-gray-500")
-                } end>
-                <div className='pl-9'>About</div>
-                </NavLink>
-                
-                <div onClick={props.bhandler} className='h-6 w-6 ml-7 hover:cursor-pointer rounded-full bg-blue-600'></div>
-                <div onClick={props.rhandler} className='h-6 w-6 ml-7 hover:cursor-pointer rounded-full bg-red-600'></div>
+        <nav style={props.mode} className='w-[100vw] fixed top-0 h-[3.5em] bg-gray-500/20 backdrop-blur-lg flex justify-between items-center'>
+            <div className='md:text-2xl text-xl font-bold p-2.5'onClick={()=>window.location.reload()} >{props.title}</div>
+            <div className='flex font-bold md:gap-5 gap-2 pr-3'>
 
-                <img src={svg} onClick={()=>{
-                    props.mhandler();
-                    handleSvg();
-                }}  alt="" className='ml-7 hover:cursor-pointer'/>
+                    <NavLink to='/' className={({ isActive }) =>
+                    props.mode.color=='black'?(isActive ? "text-black" : "text-gray-500"):(isActive ? "text-white" : "text-gray-500")} end>
+                    <div>Home</div>
+                    </NavLink>
+                    <NavLink to='/about'className={({ isActive }) =>
+                        props.mode.color=='black'?(isActive ? "text-black" : "text-gray-500"):(isActive ? "text-white" : "text-gray-500")
+                    } end>
+                    <div className=''>About</div>
+                    </NavLink>
+                    <div onClick={props.bhandler} className='h-6 w-6  hover:cursor-pointer rounded-full bg-blue-600'></div>
+                    <div onClick={props.rhandler} className='h-6 w-6  hover:cursor-pointer rounded-full bg-red-600'></div>
+                    <img src={svg} onClick={()=>{
+                        props.mhandler();
+                        handleSvg();
+                    }}  alt="" className=' hover:cursor-pointer'/>
+
             </div>
         </nav>
         </>
