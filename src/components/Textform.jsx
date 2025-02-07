82a5b3bbcb02fb879b9cfdba42f17e62e537e603
Krishna <anonymous@google.com>
2025-02-07 20:17:45 +0530

    Fixed bugs

--- src/components/Textform.jsx
+++ src/components/Textform.jsx
@@ -15,7 +15,6 @@ export default function Textform(props){
         }
         updateText(rem);
     }
-
     const updatet2=()=>{
         let upt = text.toLowerCase();
         updateText(upt);
@@ -24,7 +23,6 @@ export default function Textform(props){
         updateText(e.target.value);
         
     };
-    
     const clrfunc=()=>{
         updateText("")
     }
@@ -35,14 +33,14 @@ export default function Textform(props){
     }
     return (
         <>
-        <div style={props.mode} className='w-[100vw] min-h-screen pt-20'>
-        <div   className="p-4  w-6xl mx-auto flex items-start flex-col gap-5">
+        <div style={props.mode} className='min-h-screen py-20'>
+        <div className="p-4 w-[94vw] mx-auto flex items-start flex-col gap-5">
             <div className='flex items-start flex-col gap-2.5'>
                 <label className='md:text-3xl text-2xl font-bold pr-3' htmlFor="in">{props.title}</label>
                 <textarea type="text" name="in"value={text} id="in"onChange={changed}
                  className='overflow-hidden resize-none h-40 w-[80vw] rounded-md border-2 border-indigo-500 outline-0 p-2 '/>
             </div>
-            <div className="buttons flex  flex-wrap w-[80vw] gap-[1em]">
+            <div className="buttons flex flex-wrap w-[80vw] gap-[1em]">
                 <button onClick={clrfunc} className='bg-blue-400 text-amber-50 font-bold  md:px-7 px-4  py-1.5 rounded-2xl hover:cursor-pointer hover:bg-blue-300'>Clear text</button>
                 <button onClick={updatet}  className='bg-blue-400 text-amber-50 font-bold  md:px-7 px-4 py-1.5 rounded-2xl hover:cursor-pointer hover:bg-blue-300'>Convert to uppercase</button>
                 <button onClick={updatet2} className='bg-blue-400 text-amber-50 font-bold  md:px-7 px-4  py-1.5 rounded-2xl hover:cursor-pointer hover:bg-blue-300'>Convert to lowercase</button>
@@ -55,9 +53,9 @@ export default function Textform(props){
                 <div>{0.004*(text.trim().split(" ").filter(ele=>ele!=="").length)} minutes read.</div>
             </div>
                 <div className='md:text-3xl text-2xl font-bold'>Preview :</div>
-                <div className='w-[80vw] flex'>{text.length>0?text:"Enter the text above to preview it."}</div>
+                <div className='flex h-40'>{text.length>0?text:"Enter the text above to preview it."}</div>
         </div>
-        </div>
+    </div>
         </>
     )
 }
